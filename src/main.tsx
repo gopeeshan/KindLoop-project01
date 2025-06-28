fc22470bf25ac30a6b165b292db50dc83025a452
gopeeshan <117355487+gopeeshan@users.noreply.github.com>
2025-06-28 21:29:56 +0530

    Hero comp added

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import App from './App';
 
 const container = document.getElementById("root");
 if (!container) {
@@ -11,6 +12,6 @@ if (!container) {
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    
+    <App />
   </React.StrictMode>
 );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Navigation from "@/components/Navigation";
-
+import Hero from "@/components/Hero";
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
         <Navigation/>
+        <Hero />
     </div>
   );
 };
