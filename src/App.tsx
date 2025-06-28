35eca0bc7e9fc02a4926fb6088446f8fd227101f
gopeeshan <117355487+gopeeshan@users.noreply.github.com>
2025-06-28 21:56:51 +0530

    File update

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "@/pages/index";
-;
+import Login from "@/pages/Login";
+//import PostCreation from "@/pages/post-creation";
+//import Donations from "@/pages/donations";
 
 
 
@@ -18,6 +20,9 @@ const App = () => {
             <Sonner />
             <Routes>
               <Route path="/" element={<Index />} />
+                <Route path="/login" element={<Login />} />
+                {/* <Route path="/post-creation" element={<PostCreation />} />
+                <Route path="/donations" element={<Donations />} /> */}
             </Routes>
           </div>
         </TooltipProvider>
