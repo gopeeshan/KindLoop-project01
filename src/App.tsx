bdf97a20dd3d1cd674c2e75f79cfb734e9bfde20
gopeeshan <117355487+gopeeshan@users.noreply.github.com>
2025-06-28 20:54:34 +0530

    UI file update and App file

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+
+import React from "react";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Index from "@/pages/index";
+;
+
+
+
+const App = () => {
+  return (
+      <BrowserRouter>
+        <TooltipProvider>
+          <div className="min-h-screen">
+            <Toaster />
+            <Sonner />
+            <Routes>
+              {/* <Route path="/" element={<Index />} /> */}
+            </Routes>
+          </div>
+        </TooltipProvider>
+      </BrowserRouter>
+  );
+};
+
+export default App;
